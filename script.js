af35b4dd827dc84bdfd9a8bc998e03286ace6fb0
stewarteva918-wq <stewarteva918@gmail.com>
2026-03-03 23:38:12 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-// JavaScript для работы с Supabase
-console.log("Сайт загружен");
+const SUPABASE_URL = 'https://xqawbkilonphmhikawqs.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_84XA7ovvjzTYM4wzkvdkPg_GTRyWvOP';
